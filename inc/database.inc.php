332070a2f795ed23d6e985ebcaf2e988452fd58e
PLIROF <plirof@users.noreply.github.com>
2016-05-16 19:04:52 +0300

    Add files via upload

---
+++ inc/database.inc.php
@@ -0,0 +1,11 @@
+<?php
+//Move this file to database.inc.php
+// Database Connection details
+	$dbhost 	= "localhost"; 	// host the database is running on
+	$dbname 	= "genealogy";		// Database name
+	$dbuser 	= "john";		// Database user name
+	$dbpwd  	= "john";		// Database password
+	
+	$tblprefix 	= "family_";
+	$usepconnect	= false;
+?>
